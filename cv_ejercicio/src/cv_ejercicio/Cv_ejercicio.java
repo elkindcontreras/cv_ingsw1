d20777dca1f2de40490be1fbcb6f8bdd696f17f8
Usuario <Usuario@Usuario-PC>
2022-06-09 15:55:20 -0500

    update gh

--- cv_ejercicio/src/cv_ejercicio/Cv_ejercicio.java
+++ cv_ejercicio/src/cv_ejercicio/Cv_ejercicio.java
@@ -16,8 +16,8 @@ public class Cv_ejercicio {
      */
     public static void main(String[] args) {
         
-        System.out.println("version 0");
-        System.out.println("version 1");
+        System.out.println("version 4");
+        System.out.println("version 5");
     }
     
 }
