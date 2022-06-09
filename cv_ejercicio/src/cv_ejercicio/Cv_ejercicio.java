d2760fb2b5e4d7117c3787dfd7bc5f3d1ea63eb9
Usuario <Usuario@Usuario-PC>
2022-06-09 15:24:09 -0500

    update

--- cv_ejercicio/src/cv_ejercicio/Cv_ejercicio.java
+++ cv_ejercicio/src/cv_ejercicio/Cv_ejercicio.java
@@ -17,6 +17,7 @@ public class Cv_ejercicio {
     public static void main(String[] args) {
         
         System.out.println("version 0");
+        System.out.println("version 1");
     }
     
 }
